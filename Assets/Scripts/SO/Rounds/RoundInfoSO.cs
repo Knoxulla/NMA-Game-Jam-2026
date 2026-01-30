efe3c12fced9c71d2dff6220ffce6f4ad510efa3
Knox <87248210+Knoxulla@users.noreply.github.com>
2026-01-30 02:23:34 -0500

    imported CS

--- Assets/Scripts/SO/Rounds/RoundInfoSO.cs
+++ Assets/Scripts/SO/Rounds/RoundInfoSO.cs
@@ -8,4 +8,6 @@ public class RoundInfoSO : ScriptableObject
     public List <RewardSO> rewardPosibilities;
     public int quotaValue;
     public float timeLimit;
+    [Tooltip("What does the gacha machine say as he gives the next quota?")]public string startRoundText;
+    [Tooltip("What does he say once satiated?")] public string endRoundText;
 }
